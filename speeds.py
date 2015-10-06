b1b7d75e79c1957d6b0f0474f23635cf5bc3c380
David Bailey <dabailey@davidabailey.com>
2015-10-06 15:16:29 -0700

    added http/s functionality for speeds

--- speeds.py
+++ speeds.py
@@ -1,6 +1,9 @@
+import requests
+import io
+import zipfile
 import pandas
 import numpy
-import gtfstk
+#import gtfstk
 from shapely.geometry import Point
 from shapely.geometry import LineString
 import matplotlib.pyplot as plt
@@ -25,16 +28,30 @@ def combine(listA,listB):
     newList.extend(combine(listA[1:],listB))
     return newList
 
+class getFeed:
+  def __init__(self,url):
+    get = requests.get(url)
+    file = io.BytesIO(get._content)
+    zipFile = zipfile.ZipFile(file)
+    for name in zipFile.namelist():
+      file = io.BytesIO(zipFile.open(name).read())
+      name = name.rstrip('.txt')
+      setattr(self, name, pandas.read_csv(file))
+  def __getitem__(self, name):
+    return getattr(self, name)
+
+#  amtrak = gtfstk.feed.Feed(expanduser('~/Desktop/la-metro_20101211_0848.zip'))  #http://www.gtfs-data-exchange.com/agency/la-metro/
+# amtrak = getFeed('http://gtfs.s3.amazonaws.com/amtrak_20140723_0354.zip')
+amtrak = getFeed('http://gtfs.s3.amazonaws.com/la-metro_20101211_0848.zip')
+amtrak.trips.set_index('route_id', inplace=True)
+amtrak.trips.sort_index(inplace=True)
+amtrak.stop_times.set_index('stop_headsign', inplace=True)
+amtrak.stop_times.sort_index(inplace=True)
+amtrak.stops.set_index('stop_id', inplace=True)
+amtrak.stops.sort_index(inplace=True)
+
 def getJSONs(route,direction):
-#  amtrak = gtfstk.feed.Feed(expanduser('~/Desktop/amtrak_20140723_0354.zip')) #http://www.gtfs-data-exchange.com/agency/amtrak/
-  amtrak = gtfstk.feed.Feed(expanduser('~/Desktop/la-metro_20101211_0848.zip'))
-  amtrak.trips.set_index('route_id', inplace=True)
-  amtrak.trips.sort_index(inplace=True)
-  amtrak.stop_times.set_index('stop_headsign', inplace=True)
-  amtrak.stop_times.sort_index(inplace=True)
-  amtrak.stops.set_index('stop_id', inplace=True)
-  amtrak.stops.sort_index(inplace=True)
-#  trip_ids = amtrak.trips[(amtrak.trips['route_id'] == str(route).replace('%20', ' ')) & (amtrak.trips['direction_id'] == int(direction))]['trip_id']
+#  trip_ids = amtrak.trips[(amtrak.trips['route_id'].astype(str) == str(route).replace('%20', ' ')) & (amtrak.trips['direction_id'] == int(direction))]['trip_id']
 #  stops = pandas.merge(pandas.DataFrame(trip_ids),amtrak.stop_times)
   trip_ids = amtrak.trips.loc[str(route).replace('%20', ' ')]['trip_id']
   stops = pandas.merge(pandas.DataFrame(trip_ids),amtrak.stop_times.loc[str(direction).replace('%20',' ')])
