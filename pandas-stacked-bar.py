8289d5e0285b01aec3ef7cf32bb9a2b459a1e476
David Bailey <davidbailey@users.noreply.github.com>
2015-11-05 10:28:50 -0800

    Update pandas-stacked-bar.py

--- pandas-stacked-bar.py
+++ pandas-stacked-bar.py
@@ -1,6 +1,6 @@
 import pandas
 from matplotlib import pyplot
-df = pandas.DataFrame([431,106])
+df = pandas.DataFrame([431-106,106])
 df = df.transpose()
 df.columns=['complete','incomplete']
 df.plot(kind='bar', stacked=True, legend=False)
