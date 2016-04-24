bc954af2a5c87b80a995284626919f1d652c8c11
David Bailey <davidbailey@users.noreply.github.com>
2016-04-24 14:22:55 -0700

    Update pie.py

--- pie.py
+++ pie.py
@@ -34,18 +34,31 @@ s.close()
 # i2c-dev
 # " >> /etc/modules
 # i2cdetect -y 1
+# Based on https://github.com/dhhagan/Adafruit-Raspberry-Pi-Python-Code/blob/master/ADS1115/__init__.py
 import smbus
+from time import sleep
 bus = smbus.SMBus(1)
 address = 0x48
 
 bus.write_i2c_block_data(address, 0x01, [197,131])
+sleep(.01)
 a0 = bus.read_i2c_block_data(address, 0x00, 2)
 bus.write_i2c_block_data(address, 0x01, [213,131])
+sleep(.01)
 a1 = bus.read_i2c_block_data(address, 0x00, 2)
 bus.write_i2c_block_data(address, 0x01, [229,131])
+sleep(.01)
 a2 = bus.read_i2c_block_data(address, 0x00, 2)
 bus.write_i2c_block_data(address, 0x01, [245,131])
+sleep(.01)
 a3 = bus.read_i2c_block_data(address, 0x00, 2)
+
+out = [65535 - a0[0]*256 - a0[1], 65535 - a1[0]*256 - a1[1], 65535 - a2[0]*256 - a2[1], 65535 - a3[0]*256 - a3[1]]
+# echo "a0, a1, a2, a3" > out.csv
+f = open('out.csv', 'a')
+f.write(str(out)[1:-1] + "\n")
+f.close()
+
 
 # SPI - RC522 (13.56MHz RIFD NFC) - Connect ...
 # echo spi-bcm2708 >> /etc/modules
