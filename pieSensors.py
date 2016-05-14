400fba689f35e981de78ebc7c264127204de841b
David Bailey <davidbailey@users.noreply.github.com>
2016-05-14 16:51:30 -0700

    Update pieSensors.py

--- pieSensors.py
+++ pieSensors.py
@@ -8,6 +8,9 @@ from time import sleep
 bus = smbus.SMBus(1)
 address = 0x48
 
+import Adafruit_BMP.BMP085 as BMP085
+bmp085 = BMP085.BMP085()
+
 from time import time
 
 while(True):
@@ -27,7 +30,11 @@ while(True):
   bus.write_i2c_block_data(address, 0x01, [229,131])
   sleep(.01)
   a2 = bus.read_i2c_block_data(address, 0x00, 2)
-  out = [time(), 256 * a0[0] + a0[1], 256 * a1[0] + a1[1], 256 * a2[0] + a2[1], 256 * a3[0] + a3[1]]
+  temperature = bmp085.read_temperature()
+  pressure = bmp085.read_pressure()
+  altitude = bmp085.read_altitude()
+  sealevel_pressure = bmp085.read_sealevel_pressure()
+  out = [time(), 256 * a0[0] + a0[1], 256 * a1[0] + a1[1], 256 * a2[0] + a2[1], 256 * a3[0] + a3[1], temperature, pressure, altitude, sealevel_pressure]
   f = open('out.csv', 'a')
   f.write(str(out)[1:-1] + "\n")
   f.close()
