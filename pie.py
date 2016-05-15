775547232935a5ea8af21461d12192dd294193d1
David Bailey <davidbailey@users.noreply.github.com>
2016-05-15 15:19:14 -0700

    Update pie.py

--- pie.py
+++ pie.py
@@ -84,14 +84,15 @@ bus = smbus.SMBus(1)
 address = 0x48
 
 while(True):
-  GPIO.output(channel, GPIO.HIGH)
-  sleep(0.00028)
-  bus.write_i2c_block_data(address, 0x01, [245,131])
-  sleep(0.00004)
   GPIO.output(channel, GPIO.LOW)
+  bus.write_i2c_block_data(address, 0x01, [229,131])
+  sleep(0.00028)
+  a2 = bus.read_i2c_block_data(address, 0x00, 2)
+  sleep(0.00004)
+  GPIO.output(channel, GPIO.HIGH)
   sleep(0.0968)
-  a3 = bus.read_i2c_block_data(address, 0x00, 2)
-  print(65535 - a3[0]*256 - a3[1])
+  print(a2[0]*256 + a2[1])
+
 
 # SPI - RC522 (13.56MHz RIFD NFC) - Connect ...
 # echo spi-bcm2708 >> /etc/modules
